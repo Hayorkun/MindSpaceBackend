cfa9157a7e9806a9cc64ef050382a804dfc0638f
Ayomide Yerokun <Ayomideyerokun54@gmail.com>
2026-07-24 09:27:41 +0100

    fix: port and routing mismatch

--- server.js
+++ server.js
@@ -3,27 +3,38 @@ import UserRoutes from "./routes/UserRoute.js";
 import TaskRoutes from "./routes/TaskRoute.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173", "https://mindspace-taskmanager.vercel.app"] }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://mindspace-taskmanager.vercel.app",
+    ],
+  }),
+);
 
 app.use("/api/users", UserRoutes);
 app.use("/api/tasks", TaskRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Successfully connected to Mongo db");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   })
   .catch((error) => {
     console.log("Error connecting to Mongo db", error);
